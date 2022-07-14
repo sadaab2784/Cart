0af272400d264aa5d67fa25e13973c90de5a1788
sadaab2784 <ali.sadaab825313@gmail.com>
2022-07-14 23:10:50 +0530

    Adding products count to Navbar

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,13 +1,10 @@
 import React from "react";
 
-class CartItem extends React.Component {
-    render(){
-        console.log('this.props', this.props);
-        const { price, title, qty } = this.props.product;
-        const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = this.props;
+const CartItem = (props) => {
+        const { price, title, qty } = props.product;
+        const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = props;
         return (
             <div className="cart-item">
-                {this.props.jsx}
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
@@ -42,7 +39,6 @@ class CartItem extends React.Component {
             </div>
         );
     }
-}
 
 const styles = {
     image: {
